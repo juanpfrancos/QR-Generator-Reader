c817aae4299a65208764e4f8c315b101a7365129
Juan Pablo <juanpanime@gmail.com>
2024-05-08 23:04:09 -0500

    Add QR Reader Component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,26 @@ import { HttpClientModule } from '@angular/common/http'
 import { RouterOutlet } from '@angular/router';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 import { MaterialModuleModule } from './material-module/material-module.module';
+import { NgxScannerQrcodeModule, LOAD_WASM } from 'ngx-scanner-qrcode';
+import { JsonPipe } from '@angular/common';
+import { AsyncPipe } from '@angular/common';
+import { CommonModule } from '@angular/common';
 
+
+// Necessary to solve the problem of losing internet connection
+LOAD_WASM().subscribe();
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HttpClientModule, MaterialModuleModule, PokemonListComponent],
+  imports: [JsonPipe, AsyncPipe, CommonModule, RouterOutlet, HttpClientModule, NgxScannerQrcodeModule, MaterialModuleModule, PokemonListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'QR-Generator-Reader';
+
+  scanQRCodeSuccess(data: string) {
+    console.log('QRCode data', data);
+  }
 }
