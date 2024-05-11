76211a38e2a0661a2418ec52a055ce106ffb4c75
Juan Pablo <juanpanime@gmail.com>
2024-05-10 22:37:39 -0500

    Add Set back camera

--- src/app/scanncode/scanncode.component.ts
+++ src/app/scanncode/scanncode.component.ts
@@ -1,5 +1,6 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ScannModuleModule } from '../scann-module/scann-module.module'; 
+import { Component, ViewChild } from '@angular/core';
+import { ZXingScannerComponent } from '@zxing/ngx-scanner';
 
 @Component({
   selector: 'app-scanncode',
@@ -7,6 +8,28 @@ import { ScannModuleModule } from '../scann-module/scann-module.module';
   imports: [ScannModuleModule],
   templateUrl: './scanncode.component.html',
 })
-export class ScanncodeComponent{
+export class ScanncodeComponent {
+  @ViewChild('scanner', { static: false })
+  scanner!: ZXingScannerComponent;
 
+  scannerEnabled: boolean = false;
+  desiredDevice: any; // This will hold the selected device for scanning
+
+  toggleScanner(): void {
+    this.scannerEnabled = !this.scannerEnabled;
+  }
+
+  onCamerasFound(devices: MediaDeviceInfo[]): void {
+    // If there are multiple cameras, set the desiredDevice to the back camera
+    const backCamera = devices.find(device => device.label.toLowerCase().includes('back'));
+    if (backCamera) {
+      this.desiredDevice = backCamera;
+    } else {
+      this.desiredDevice = devices[0]; // If no back camera, use the first one found
+    }
+  }
+
+  scanSuccessHandler(result: string): void {
+    console.log('QR Code scanned:', result);
+  }
 }
