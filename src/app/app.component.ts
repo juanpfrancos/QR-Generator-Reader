61cc59cecb7236aabfe1efa2b785520d9ce4df76
Juan Pablo <juanpanime@gmail.com>
2024-05-12 01:53:07 -0500

    Generate QR.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,19 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from './shared.module';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common'
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, CommonModule, SharedModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [
+    // ...otros proveedores...
+    {provide: LocationStrategy, useClass: HashLocationStrategy}
+  ]
 })
 export class AppComponent {
 
--- src/app/shared.module.ts
+++ src/app/shared.module.ts
@@ -2,6 +2,7 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common'
 
 @NgModule({
     imports: [
@@ -10,6 +11,10 @@ import { CommonModule } from '@angular/common';
     ],
     exports: [
         HttpClientModule
-    ]
+    ],
+    providers: [
+        // ...otros proveedores...
+        {provide: LocationStrategy, useClass: HashLocationStrategy}
+      ]
 })
 export class SharedModule { }
